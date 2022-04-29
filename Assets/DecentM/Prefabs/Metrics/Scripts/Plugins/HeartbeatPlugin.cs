c3fcb417e1322db343ce4c7ed6e15b6079227bea
DecentM <decentm@decentm.com>
2022-04-29 03:36:31 +0300

    feat(Metrics): create interaction and trigger volume plugins
    Former-commit-id: 5f775481d5c89b552bc80920df36ee792a7810cd

---
+++ Assets/DecentM/Prefabs/Metrics/Scripts/Plugins/HeartbeatPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace DecentM.Metrics.Plugins
+{
+    public class HeartbeatPlugin : MetricsPlugin
+    {
+        public int reportingIntervalSeconds = 60;
+
+        public float elapsed = 0;
+        public bool initialised = false;
+
+        private bool isVr = false;
+        private int timezone = 0;
+
+        protected override void _Start()
+        {
+            TimeZoneInfo tz = TimeZoneInfo.Local;
+            TimeSpan offset = tz.BaseUtcOffset;
+            this.timezone = offset.Hours;
+
+            this.isVr = Networking.LocalPlayer.IsUserInVR();
+        }
+
+        private void DoHeartbeat()
+        {
+            VRCUrl url = this.urlStore.GetHeartbeatUrl(Networking.LocalPlayer.isMaster, this.isVr, this.timezone);
+            if (url == null) return;
+
+            this.system.RecordMetric(url, Metric.Heartbeat);
+        }
+
+        protected override void OnMetricsSystemInit()
+        {
+            this.DoHeartbeat();
+            this.initialised = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Networking.LocalPlayer == null || !Networking.LocalPlayer.IsValid() || !this.initialised) return;
+
+            this.elapsed += Time.fixedUnscaledDeltaTime;
+            if (this.elapsed <= this.reportingIntervalSeconds) return;
+            this.elapsed = 0;
+
+            this.DoHeartbeat();
+        }
+    }
+}
