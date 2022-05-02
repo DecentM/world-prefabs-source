7d5d9a52bb7bc9a3c1e2e932d8154512874461e7
DecentM <decentm@decentm.com>
2022-05-02 04:00:32 +0300

    feat(VideoPlayer): generate thumbnail files for YouTube URLs
    Former-commit-id: fde4d0399dfbcd441d1b82f11ef31f08a40cd87e
    Former-commit-id: 0d201eb9d46c40cf0d55d404cbc3a9d3cc86bbcb
    Former-commit-id: 885387e4f6115a755e293b02d51f0e688c7e9b98

---
+++ Assets/DecentM/Prefabs/VideoPlayer/Scripts/Editor/VideoPlayerAutoFixer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using DecentM.EditorTools;
+using DecentM.VideoPlayer.Plugins;
+
+namespace DecentM.VideoPlayer
+{
+    public class VideoPlayerAutoFixer : AutoSceneFixer
+    {
+        protected override bool OnPerformFixes()
+        {
+            ComponentCollector<VideoPlayerUI> collector = new ComponentCollector<VideoPlayerUI>();
+            List<VideoPlayerUI> players = collector.CollectFromActiveScene();
+
+            foreach (VideoPlayerUI player in players)
+            {
+                VideoPlayerPlugin[] plugins = player.GetComponentsInChildren<VideoPlayerPlugin>();
+                PluginRequirements requirements = PluginManager.GetRequirements(player);
+
+                foreach (VideoPlayerPlugin plugin in plugins)
+                {
+                    plugin.events = requirements.events;
+                    plugin.system = requirements.system;
+                    plugin.pubsubHosts = new Pubsub.PubsubHost[] { requirements.events };
+
+                    Inspector.SaveModifications(plugin);
+                }
+            }
+
+            return true;
+        }
+    }
+}
