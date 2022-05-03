e1e112cbe6b5fa2ead7ac4bf721ae172fdc6bf84
DecentM <decentm@decentm.com>
2022-05-03 11:14:20 +0300

    feat(VideoPlayer): create crude ingame UI for playlists
    Former-commit-id: f949916ee287970a24aa564573cceb70ea915dcb
    Former-commit-id: 9bf009180302bac9a1831062dd93b00a23139d4e
    Former-commit-id: 81e7129ad64d74ef8ac6e63402dd73ccb53a8fc2

---
+++ Assets/DecentM/Prefabs/VideoPlayer/Scripts/Plugins/Playlist/PlaylistPlayerPlugin.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components.Video;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace DecentM.VideoPlayer.Plugins
+{
+    public class PlaylistPlayerPlugin : VideoPlayerPlugin
+    {
+        public VideoPlaylist playlist;
+
+        protected override void OnVideoPlayerInit()
+        {
+            VRCUrl nextUrl = this.playlist.GetCurrentUrl();
+            if (nextUrl == null) return;
+
+            this.system.LoadVideo(nextUrl);
+        }
+
+        protected override void OnPlaybackEnd()
+        {
+            VRCUrl nextUrl = this.playlist.Next();
+            if (nextUrl == null) return;
+
+            this.system.LoadVideo(nextUrl);
+        }
+    }
+}
