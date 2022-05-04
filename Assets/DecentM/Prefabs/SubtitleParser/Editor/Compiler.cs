2015e8ff7883d66fc5b51b3b89d3bcacc5e1e427
DecentM <decentm@decentm.com>
2022-05-04 15:33:25 +0300

    feat(Subtitles): implement vtt format support
    Former-commit-id: 4fafe173c53d4281bb9f29892baa2283873c1a68
    Former-commit-id: 58cc9b2a24e0e6edd5088f6c6bcdd9eebe07d092
    Former-commit-id: a7d224caad1347ec772ee2869c2b4593177e56ca

---
+++ Assets/DecentM/Prefabs/SubtitleParser/Editor/Compiler.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecentM.Subtitles
+{
+    class FileTypes
+    {
+        public const string Srt = ".srt";
+        public const string Ass = ".ass";
+        public const string Usf = ".usf";
+        public const string Vtt = ".vtt";
+        public const string Stl = ".stl";
+        public const string Sub = ".sub";
+        public const string Ssa = ".ssa";
+        public const string Ttxt = ".ttxt";
+
+        public static bool IsSupported(string filetype)
+        {
+            return FileTypes.SupportedFormats.Any(f => f == filetype);
+        }
+
+        public static string[] SupportedFormats = { Srt, Vtt };
+    }
+
+    public class Compiler
+    {
+        private Srt.Lexer srtLexer = new Srt.Lexer();
+        private Srt.Parser srtParser = new Srt.Parser();
+        private Srt.Transformer srtTransformer = new Srt.Transformer();
+
+        private Vtt.Lexer vttLexer = new Vtt.Lexer();
+        private Vtt.Parser vttParser = new Vtt.Parser();
+        private Vtt.Transformer vttTransformer = new Vtt.Transformer();
+
+        public struct CompilationResultError
+        {
+            public string value;
+
+            public CompilationResultError(string value = "")
+            {
+                this.value = value;
+            }
+        }
+
+        public struct CompilationResult
+        {
+            public string output;
+            public List<CompilationResultError> errors;
+
+            public CompilationResult(List<CompilationResultError> errors, string output = "")
+            {
+                this.output = output;
+                this.errors = errors;
+            }
+        }
+
+        public CompilationResult Compile(string source, string fileType)
+        {
+            CompilationResult result = new CompilationResult(new List<CompilationResultError>(), "");
+            List<Instruction> instructions = new List<Instruction>();
+
+            if (!FileTypes.IsSupported(fileType))
+            {
+                throw new NotImplementedException($"File type {fileType} is not supported. Use a different file, or convert your subtitles to a supported format: {String.Join(", ", FileTypes.SupportedFormats)}");
+            }
+
+            string sanitisedSource = new TextProcessing(source)
+                .CRLFToLF()
+                .ResolveHTMLEntities()
+                .GetResult();
+
+            switch (fileType)
+            {
+                case FileTypes.Srt:
+                    {
+                        List<Srt.Lexer.Token> tokens = this.srtLexer.Lex(sanitisedSource);
+                        Srt.Parser.Ast ast = this.srtParser.Parse(tokens);
+                        List<Srt.Parser.Node> errors = Srt.Parser.GetUnknowns(ast);
+
+                        // Add all errors to the result struct so they can be displayed to the user
+                        if (errors.Count != 0)
+                        {
+                            errors.ForEach(error =>
+                            {
+                                if (error.value != null)
+                                {
+                                    result.errors.Add(new CompilationResultError((error.value.ToString())));
+                                }
+                            });
+                        }
+
+                        instructions = this.srtTransformer.ToInstructions(ast);
+                        break;
+                    }
+
+                case FileTypes.Ass:
+                case FileTypes.Usf:
+                case FileTypes.Vtt:
+                    {
+                        List<Vtt.Lexer.Token> tokens = this.vttLexer.Lex(sanitisedSource);
+                        Vtt.Parser.Ast ast = this.vttParser.Parse(tokens);
+                        List<Vtt.Parser.Node> errors = Vtt.Parser.GetUnknowns(ast);
+
+                        // Add all errors to the result struct so they can be displayed to the user
+                        if (errors.Count != 0)
+                        {
+                            errors.ForEach(error =>
+                            {
+                                if (error.value != null)
+                                {
+                                    result.errors.Add(new CompilationResultError((error.value.ToString())));
+                                }
+                            });
+                        }
+
+                        instructions = this.vttTransformer.ToInstructions(ast);
+                        break;
+                    }
+                case FileTypes.Stl:
+                case FileTypes.Sub:
+                case FileTypes.Ssa:
+                case FileTypes.Ttxt:
+                default:
+                    break;
+            }
+
+            result.output = "";
+
+            instructions.ForEach((Instruction instruction) =>
+            {
+                result.output += instruction.ToString();
+                result.output += '\n';
+            });
+
+            return result;
+        }
+    }
+}
+
